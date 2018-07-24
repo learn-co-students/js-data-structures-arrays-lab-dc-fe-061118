68214f583bab2e6c9659fcace5a35649ad49c791
Christine Patton <christine@pattoncreative.com>
2018-07-24 20:40:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Write your solution here!
+const drivers=['Milo', 'Otis', 'Garfield'];
+console.log(drivers);
+
+function destructivelyAppendDriver(name) {
+  drivers.push('Ralph')
+}
+
+function destructivelyPrependDriver(name) {
+  drivers.unshift('Bob')
+}
+
+function destructivelyRemoveLastDriver(name) {
+  drivers.pop()
+}
+
+function destructivelyRemoveFirstDriver(name) {
+  drivers.shift()
+}
+
+
+function appendDriver (name) {
+  const driversPlusAppend = [...drivers, 'Broom'];
+  return (driversPlusAppend)
+}
+
+function prependDriver (name) {
+  const driversPlusPrepend = ['Arnold', ...drivers];
+  return (driversPlusPrepend)
+}
+
+function removeLastDriver (name) {
+  const driversMinusLast = drivers.slice(0,2);
+  return (driversMinusLast)
+}
+
+function removeFirstDriver (name) {
+  const driversMinusFirst = drivers.slice(1);
+  return (driversMinusFirst)
+}
+
+
+
+
